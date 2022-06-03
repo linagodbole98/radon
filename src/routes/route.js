c5bcbb1f0e8c17d3edc9b85d96988c282a604baf
linagodbole98 <linagodbole98@gmail.com>
2022-06-03 11:17:00 +0530

    assign

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,4 @@
-const express = require('express');
+ const express = require('express');
 const externalModule = require('../logger/logger') //handler and controller
 const formatterModule = require('../validator/formatter')
 const helperModule = require('../util/helper')
@@ -6,20 +6,60 @@ const router = express.Router();
 
 
 
-router.get('/test-me', function (req, res) { 
-    console.log('--------this is assignment 1-----------')
-    externalModule.welcome()
-    console.log('--------this is assignment 2-----------')
-    helperModule.date()
-    helperModule.month()
-    helperModule.getBatchInfo()
-    console.log('--------this is assignment 3-----------')
-    formatterModule.trim()
-    formatterModule.tolowercase()
-    formatterModule.touppercase()
-   
-    res.send('my todays assignment Done')
+//const { Router } = require('express');
+//const express = require('express');
+
+let movies=['Rang de basanti',
+'The shining',
+'Lord of the Rings',
+'Batman begins'];
+router.get("/movies",function(req,res){
+    res.send(movies);
+})
+router.get("movies/:indexNumber",function(req,res){
+    let index=req.params.indexNumber;
+    if(movies[index]===undefined){
+    res.send("request you to see valid index!")}
+    res.send(movies[index]);
+})
+let films=[
+    {
+        id:1,name:"The shining"
+    },
+    {
+        id:2,name:"Incending"
+    },
+    {
+        id:3,name:"Rang de basanti"
+    },
+    {
+        id:4, name:"Batman begins"
+    },
+];
+
+router.get("/films",function(req,res){
+    res.send(films);
+})
+
+router.get("films/:filmId",function(req,res){
+    let filmId=req.params.filmId;
+    let output=films.find((x) =>x.id===filmId);
+    if (output ===undefined) res.send("no movies")
+    res.send(output);
+
+
+});
+
+router.get('/profile/:start/:end', function (req, res) {
+    console.log("Starting Page: ", req.params['start']);
+    console.log("Ending Page: ", req.params['end']);
+    res.send("hello dosto");
+})
+
+router.get('/hlw/:name',function(req,res){
+    //console.log("starting name",req.params['name'],'linaaa');
+    req.send("starting name",req.params['name']);
 });
 
 module.exports = router;
-// adding this comment for no reason
+
