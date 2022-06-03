f0c1fb7ebaa3b2b1f2ba1270d3abd94a30ce2e5f
linagodbole98 <linagodbole98@gmail.com>
2022-06-03 08:15:52 +0530

    UPDATE

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const _ = require("lodash")
+const_ = require("lodash")
 const externalModule = require('../logger/logger') //handler and controller
 const formatterModule = require('../validator/formatter')
 const helperModule = require('../util/helper');
@@ -29,40 +29,41 @@ console.log("Before: ", arr)
   
 // Making chunks of size 3
 console.log("After: ", _.chunk(arr, 3))
-});
 
-router.get('/tail',function(req,res){
-    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19,20];
+
+
+    const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19,20];
    
 
-    const odds = arr.filter(number => {
+    const odds = arr1.filter(number => {
       return number % 2 !== 0;
     });
     
     console.log(_.tail(odds));
 
-})
 
 
-router.get('/union',function(req,res){
-    const _ = require("lodash");  
+
+
+    //const _ = require("lodash");  
   
 // Use of _.union() method 
-let gfg = _.union([20, 12,6,15], [8, 15, 6]); 
+let gfg = _.union([20, 12,6,15], [8, 15, 6],[6,8,9],[9,8,5],[9,6,7]); 
         
 // Printing the output  
 console.log(gfg)
-});
 
-router.get('/frompair',function(req,res){
-    const _ = require('lodash');
+
+
+   // const _ = require('lodash');
   
 let pairs = [['x', 1], ['y', 2], ['z', 3]]
   
 let obj = _.fromPairs(pairs);
   
 console.log(obj)
-})
+});
+
 
 module.exports = router;
 // adding this comment for no reason
