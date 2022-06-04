d704276ff92aeaabba596025c3714a7f351a1fdd
linagodbole98 <linagodbole98@gmail.com>
2022-06-04 11:44:49 +0530

    demo

--- src/routes/route.js
+++ src/routes/route.js
@@ -5,6 +5,41 @@ const helperModule = require('../util/helper')
 const router = express.Router();
 
 
+//---------------------------------assignment number-3------------------------------------
+let players = []
+
+
+
+
+router.post('/players', function (req, res) {
+    
+    let newPlayer = req.body
+    let newPlayersName = newPlayer.name
+    let isNameRepeated = false
+
+    //let player = players.find(p => p.name == newPlayersName)
+
+    for(let i = 0; i < players.length; i++) {
+        if(players[i].name == newPlayersName) {
+            isNameRepeated = true;
+            break;
+        }
+    }
+
+    //undefined is same as false/ a falsy value
+    if (isNameRepeated) {
+        //Player exists
+        res.send("This player was already added!")
+    } else {
+        //New entry
+        players.push(newPlayer)
+        res.send(players)
+    }
+});
+
+
+//-------------------------------------done-----------------------------------------n
+
 
 
 router.get('/hlw/:name',function(req,res){
